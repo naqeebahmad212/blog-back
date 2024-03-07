763eabc4e5d8fe190aed644d3356798759c2f79a
naqeebahmad212 <naqeebahmad212@gmail.com>
2024-03-07 12:02:47 +0500

    mogno

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require(`mongoose`);
 const express = require(`express`);
 const app = express();
 const URI =
-  "mongodb://soomrush212:ahmadjan1@ac-ux15x8w-shard-00-00.i0gyfov.mongodb.net:27017,ac-ux15x8w-shard-00-01.i0gyfov.mongodb.net:27017,ac-ux15x8w-shard-00-02.i0gyfov.mongodb.net:27017/mern-blo?ssl=true&replicaSet=atlas-v26p2r-shard-0&authSource=admin&retryWrites=true&w=majority";
+  "mongodb+srv://soomrush212:ahmadjan1@my-cluster.i0gyfov.mongodb.net/y-app?retryWrites=true&w=majority";
 
 const connectDB = async () => {
   try {
